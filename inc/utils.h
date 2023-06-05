bbbfde5d82d46d5a8e5d5731c89cd1da2dd4c8b3
Santiago Magniny <smagniny@c1r8s6.42madrid.com>
2023-06-05 10:59:21 +0200

    save before pull

--- inc/utils.h
+++ inc/utils.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smagniny <santi.mag777@student.42madrid    +#+  +:+       +#+        */
+/*   By: smagniny <smagniny@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 20:17:25 by smagniny          #+#    #+#             */
-/*   Updated: 2023/06/01 16:48:22 by smagniny         ###   ########.fr       */
+/*   Updated: 2023/06/02 12:30:22 by smagniny         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	sb(t_Stack *stack_b);
 void	ra(t_Stack *stack_a);
 void	rb(t_Stack *stack_b);
 void	reverse_rotate(t_Stack *stack);
-void	push_a_to_b(t_Stack *stack_a, t_Stack *stack_b);
-void	push_b_to_a(t_Stack *stack_a, t_Stack *stack_b);
+void	pb(t_Stack *stack_a, t_Stack *stack_b);
+void	pa(t_Stack *stack_a, t_Stack *stack_b);
 //Linked List funcs
 t_Node	*lastelem(t_Stack	*stack);
 void	printstack(t_Stack *stack, char id);
@@ -52,7 +52,7 @@ int		check_order(t_Stack *stack);
 int		check_dupli_manage(t_Stack *stack);
 //algo
 void	quickSortLinkedList(t_Node** head, t_Node** tail);
-void	panic(t_Stack	*stack);
+void	panic(t_Stack	*stack, int error);
 
 void	chainfilter(t_Stack *stack_a, t_Stack *stack_b);
 #endif
